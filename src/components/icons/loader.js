4c485d19e0d34b60377ddc8f0e9d9442e971e057
Rahul Pulidindi <rahul025@uw.edu>
2020-10-20 16:33:32 -0400

    Some logo changes

--- src/components/icons/loader.js
+++ src/components/icons/loader.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const IconLoader = () => (
-  <svg id="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+  <svg id="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
     <title>Loader Logo</title>
     <g>
       <g id="B" transform="translate(11.000000, 5.000000)">
